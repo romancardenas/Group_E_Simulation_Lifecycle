21791ee6a7c7b910ffba75b29da94efa90e0eb9d
Román Cárdenas <rcardenas.rod@gmail.com>
2020-10-13 21:05:37 +0200

    Project setup

---
+++ include/simulation_lifecycle/workflow/workflow.c
@@ -0,0 +1,5 @@
+//
+// Created by Román Cárdenas Rodríguez on 13/10/2020.
+//
+
+#include "workflow.h"
---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Simulation Lifecycle: main function. So far, it just prints "Hello, World!"
+ * @param argc number of arguments
+ * @param argv value of the arguments
+ * @return always 0
+ */
+int main(int argc, char *argv[]) {
+    puts("Hello, World!");
+    return 0;
+}
