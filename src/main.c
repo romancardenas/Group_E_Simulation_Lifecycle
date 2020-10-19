0eab7e6eb5c64e6aee11f83093ae5d6a9d00d3b1
Román Cárdenas <rcardenas.rod@gmail.com>
2020-10-19 13:42:13 +0200

    Added support to GDAL in CMakeLists.txt

--- src/main.c
+++ src/main.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
+#include <gdal.h>
+#include <cpl_conv.h>
 
 /**
- * Simulation Lifecycle: main function. So far, it just prints "Hello, World!"
+ * Simulation Lifecycle: main function. So far, it i just a simple example to check if GDAL is installed properly.
  * @param argc number of arguments
  * @param argv value of the arguments
  * @return always 0
  */
 int main(int argc, char *argv[]) {
-    puts("Hello, World!");
-    return 0;
+    char *pszFilename = "../data/example.geojson";
+    GDALDatasetH hDataSet;
+
+    if (argc != 2) {
+        fprintf(stderr, "Wrong number of arguments. Usage: %s PATH_TO_RASTER_IMAGE\n", argv[0]);
+        return -1;
+    }
+
+    GDALAllRegister();
+    hDataSet = GDALOpen(argv[1], GA_ReadOnly);
+    if (hDataSet != NULL) {
+        puts("Input Raster read successfully");
+        return 0;
+    } else {
+        fprintf(stderr, "Unable to read raster file %s\n", argv[1]);
+        return -1;
+    }
 }
