b2457eb5ddd550bce1087a565d192dd6c0033409
Román Cárdenas <rcardenas.rod@gmail.com>
2020-10-27 18:41:14 +0100

    GDAL removed. First version of simulation.h file

--- include/simulation_lifecycle/workflow/workflow.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by Román Cárdenas Rodríguez on 13/10/2020.
-//
-
-#include "workflow.h"
--- src/main.c
+++ src/main.c
@@ -1,29 +1,12 @@
 #include <stdio.h>
-#include <gdal.h>
-#include <cpl_conv.h>
 
 /**
- * Simulation Lifecycle: main function. So far, it i just a simple example to check if GDAL is installed properly.
+ * Simulation Lifecycle: main function. So far, it i just a simple hello world.
  * @param argc number of arguments
  * @param argv value of the arguments
  * @return always 0
  */
 int main(int argc, char *argv[]) {
-    char *pszFilename = "../data/example.geojson";
-    GDALDatasetH hDataSet;
-
-    if (argc != 2) {
-        fprintf(stderr, "Wrong number of arguments. Usage: %s PATH_TO_RASTER_IMAGE\n", argv[0]);
-        return -1;
-    }
-
-    GDALAllRegister();
-    hDataSet = GDALOpen(argv[1], GA_ReadOnly);
-    if (hDataSet != NULL) {
-        puts("Input Raster read successfully");
-        return 0;
-    } else {
-        fprintf(stderr, "Unable to read raster file %s\n", argv[1]);
-        return -1;
-    }
+    puts("Hello, world!");
+    return 0;
 }
