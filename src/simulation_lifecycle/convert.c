5fe5b597fdb70b3b798f98669f53f465787724c2
staubibr <staubin.bruno@gmail.com>
2020-12-11 06:35:02 -0500

    Fixed compatibility issue in convert.c Fixed output folder issue in test_visualization.c

--- src/simulation_lifecycle/convert.c
+++ src/simulation_lifecycle/convert.c
@@ -10,7 +10,7 @@
 #define NAME_MAX 255
 #define PATH_MAX 4096
 #define LENGTH_MAX 200
-#define VALID_FORMAT "[cadmium::celldevs::cell_ports_def<std::string"
+// #define VALID_FORMAT "[cadmium::celldevs::cell_ports_def<std::string"
 /*
  * This is compiler-dependant. For example, we should do this (and this is not 100% true, as you can use different compilers):
 #if defined __APPLE__
@@ -124,9 +124,9 @@ int convert_results(char *input, char *output, cJSON *visualization) {
          * with "[cadmium::celldevs::cell_ports_def<std::string"*/
         // TODO (Román) Careful with this!!! It is highly dependent on the compiler (see the macro definitions)
         // TODO (Román) Can we just remove this comparison? it is too narrow, in my opinion
-        if (strncmp(line, VALID_FORMAT, strlen(VALID_FORMAT)) != 0) {
-            return CONVERT_FILE_FORMAT_INCORRECT;
-        }
+        // if (strncmp(line, VALID_FORMAT, strlen(VALID_FORMAT)) != 0) {
+        //    return CONVERT_FILE_FORMAT_INCORRECT;
+        //}
 
         fclose(f_input_txt);
 
@@ -145,7 +145,6 @@ int convert_results(char *input, char *output, cJSON *visualization) {
             return res;
         }
 
-        // TODO: I think just checking if the 2 files exist at the output location is sufficient.
         /* Verifying that the results folder now contains the
          * converted results files.
          * Results folder should now contain:
